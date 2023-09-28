bb1381b481fd676e263106a2ee3fc9bc7cc03a76
Omkar0903 <104353169+Omkar0903@users.noreply.github.com>
2023-09-28 15:26:54 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suyash Shingote - Personal Website</title>
+    
+    <!-- Add Bootstrap CSS link -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <!-- Add your custom CSS (style.css) -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Suyash Shingote</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Header -->
+<header class="bg-primary text-white text-center py-5">
+    <div class="container">
+        <h1 class="display-4">Suyash Shingote</h1>
+        <p class="lead">3rd Year BCA Student | Python | Java | PHP</p>
+    </div>
+</header>
+
+<!-- About Me -->
+<section id="about" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>About Me</h2>
+                <p>I am a 3rd-year Bachelor in Computer Application (BCA) student at Savitribai Phule Pune University. I have expertise in Python, Java, PHP, and a basic knowledge of C and C++. Passionate about coding and software development.</p>
+            </div>
+            <div class="col-lg-6">
+                <img src="T:/Suyash Shingote git pages/Photos/SSLOGO.png" alt="Suyash Shingote" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Portfolio -->
+<section id="portfolio" class="bg-light py-5">
+    <div class="container">
+        <h2 class="text-center">Portfolio</h2>
+        <div class="row">
+            <!-- Add your portfolio items here using Bootstrap cards or other components -->
+        </div>
+    </div>
+</section>
+
+<!-- Contact Me -->
+<section id="contact" class="py-5">
+    <div class="container">
+        <h2 class="text-center">Contact Me</h2>
+        <p class="text-center">Email: <a href="mailto:shingotesuyashwork@gmail.com">shingotesuyashwork@gmail.com</a><br>Mobile No.: 7871873633</p>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+        <p>&copy; 2023 Suyash Shingote</p>
+    </div>
+</footer>
+
+<!-- Add Bootstrap JS and jQuery (required for Bootstrap components) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
